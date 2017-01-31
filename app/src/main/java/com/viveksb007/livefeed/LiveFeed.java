d67739f801af233dafa4a2dd962b54fe4b0f35b8
Vivek Singh Bhadauria <viveksbhadauria007@gmail.com>
2017-01-31 22:29:54 +0530

    Not Tested yet

--- app/src/main/java/com/viveksb007/livefeed/LiveFeed.java
+++ app/src/main/java/com/viveksb007/livefeed/LiveFeed.java
@@ -2,10 +2,23 @@ package com.viveksb007.livefeed;
 
 import android.app.Activity;
 import android.hardware.Camera;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.FrameLayout;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -17,8 +30,11 @@ import butterknife.ButterKnife;
 @SuppressWarnings("deprecation")
 public class LiveFeed extends Activity {
 
+    private final String TAG = "LiveFeed";
     private Camera mCamera;
     private CameraPreview mCameraPreview;
+    ServerSocket serverSocket;
+    byte[] imageData;
 
     @BindView(R.id.camera_preview)
     FrameLayout camPreview;
@@ -32,6 +48,8 @@ public class LiveFeed extends Activity {
 
         setContentView(R.layout.live_feed);
         ButterKnife.bind(this);
+
+        Log.v("IP_ADDRESS", getIpAddress());
 
         mCamera = getCameraInstance();
         mCameraPreview = new CameraPreview(this, mCamera);
@@ -56,5 +74,94 @@ public class LiveFeed extends Activity {
     protected void onDestroy() {
         super.onDestroy();
         mCamera.stopPreview();
+        mCamera.release();
+    }
+
+    private Camera.PictureCallback mPicture = new Camera.PictureCallback() {
+        @Override
+        public void onPictureTaken(byte[] bytes, Camera camera) {
+            // bytes is image data that needs to transfer to desktop
+            imageData = bytes;
+        }
+    };
+
+    public class ServerSocketThread extends Thread {
+        static final int SOCKET_SERVER_PORT = 8080;
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            try {
+                serverSocket = new ServerSocket(SOCKET_SERVER_PORT);
+                while (true) {
+                    socket = serverSocket.accept();
+                    SendFrame sendFrame = new SendFrame(socket);
+                    sendFrame.start();
+                }
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    public class SendFrame extends Thread {
+        Socket socket;
+
+        SendFrame(Socket socket) {
+            this.socket = socket;
+        }
+
+        @Override
+        public void run() {
+            try {
+                ObjectOutputStream outputStream = new ObjectOutputStream(socket.getOutputStream());
+                mCamera.takePicture(null, mPicture, null);
+                outputStream.writeObject(imageData);
+                outputStream.flush();
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    private String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "SiteLocalAddress: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+                }
+            }
+        } catch (SocketException e) {
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+        return ip;
     }
 }
